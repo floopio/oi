ac82400bfdbe324298369aee22ccd49632609a2e
Ryan Thomas <rthomas@atlassian.com>
2014-03-27 17:03:09 +1100

    Added heartbeat and set-state and heartbeat thread for client

--- src/oi/client/service.clj
+++ src/oi/client/service.clj
@@ -4,16 +4,51 @@
             [figgus.core :as cfg]
             [org.httpkit.client :as http]))
 
-(def node-id (atom nil))
+(def service-name (ref nil))
+(def node-id (ref nil))
+(def service-state (ref nil))
 
-(defn register-service [service-name metadata]
+(defn heartbeat []
+  "Function to send the hearbeat message with the state of the instance. 
+   This function will block."
+  (dosync (when-not @node-id
+            (throw (IllegalStateException. "You must call register-service fisrt"))))
+  (http/put (str (cfg/get "client.master") "/api/service/" @service-name "/" @node-id)
+            {:content-type "application/json"
+             :body (generate-string {:state @service-state})}
+            (fn [{:keys [status headers body error]}]
+              (log/debug "Status:" status)))
+  true)
+
+(defn- heartbeater []
+  "Function for sending heartbeats to the master."
+  (log/debug "Heartbeat thread -" @node-id "-" @service-state)
+  (when (heartbeat)
+    (Thread/sleep (cfg/get "client.heartbeatms" 4000))
+    (recur)))
+
+(defn register-service [name metadata]
+  (dosync (when @service-name
+            (throw (IllegalStateException. "Register has already been called - this can only be called once.")))
+          (alter service-name (fn [_] name)))
   (if @node-id
     (do (log/warn "This node is already registered with ID " @node-id " you cannot register it again.")
         @node-id)
-    (http/put (str (cfg/get "client.master") "/api/service/" service-name)
+    (http/put (str (cfg/get "client.master") "/api/service/" name)
               {:content-type "application/json"
                :body (generate-string {:metadata metadata})}
               (fn [{:keys [status headers body error]}]
                 (if (not (= 201 status))
                   (log/error "Unable to register node!")
-                  (swap! node-id (fn [_] (get (parse-string body true) :instance-id))))))))
+                  (do
+                    (dosync
+                     (alter node-id (fn [_] (get (parse-string body true) :instance-id)))
+                     (alter service-state (fn [_] :starting)))
+                    (log/info "Starting heartbeat thread")
+                    (future (heartbeater))))))))
+
+(defn set-state [state]
+  "Function to set the state of the service instance on next heartbeat."
+  (log/info "Setting instance state to" state "-" @node-id)
+  (dosync (alter service-state (fn [_] state)))
+  nil)
